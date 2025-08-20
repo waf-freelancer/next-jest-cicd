833ec607f06384f6dde0c7bde5da22f085b08e6a
Waf <waf.freelancer@gmail.com>
2025-08-20 21:33:49 +0700

    initial commit

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,18 @@
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function generateMetadata({ params }: Params) {
+  return { title: `Post: ${params.slug}` };
+}
+
+export default function Page({ params }: Params) {
+  return (
+    <h1>
+      Slug: {params.slug}
+      <span>Hello world</span>
+    </h1>
+  );
+}
