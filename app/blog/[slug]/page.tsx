100451a08da4c90b292dd9c66bf4366e52b7a598
Waf <waf.freelancer@gmail.com>
2025-08-29 21:22:35 +0700

    refactor: simplify slug extraction in Page component by changing params type from Promise to direct object

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 // page.tsx
 import PageContent from "./components/PageContent";
 
-export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
-  const { slug } = await params;
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { slug } = params;
   return <PageContent slug={slug} />;
 }
